4bbc9ec724793278b124562a0f02b8f438648e81
jbd <bendaj11@gmail.com>
2026-07-07 15:52:31 +0300

    added host

---
+++ apps/mobile-host/atlas.config.ts
@@ -0,0 +1,7 @@
+import type { AtlasConfig } from "@atlas/contracts" with { "resolution-mode": "import" };
+
+export default {
+  id: "mobile-host",
+  name: "Mobile Host",
+  framework: "angular"
+} satisfies AtlasConfig;
